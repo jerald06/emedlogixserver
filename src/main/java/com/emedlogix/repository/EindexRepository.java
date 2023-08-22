5ea4a108ca16c02fe442be9b80d7c24a59f0e9ad
vdhanabalan <vdhanabalan@ghx.com>
2023-08-22 18:45:42 +0530

    Added query changes

--- src/main/java/com/emedlogix/repository/EindexRepository.java
+++ src/main/java/com/emedlogix/repository/EindexRepository.java
@@ -19,21 +19,21 @@ public interface EindexRepository extends JpaRepository<Eindex, Integer> {
 			+ "join term_hierarchy t on t.parent_id=e.id where t.child_id = :id order by t.level asc", nativeQuery = true)
 	List<Map<String,Object>> getParentChildList(Integer id);
 
-	@Query(value = "select e.id as id,e.title as title,e.code as code,e.see as see,e.seealso as seealso,e.seecat as seecat,e.nemod as nemod,e.ismainterm as ismainterm from eindex e "
+	@Query(value = "select e.id as id,e.title as title,e.code as code,e.see as see,e.seealso as seealso,e.seecat as seecat,e.nemod as nemod,e.ismainterm as ismainterm,t.child_id as childId, t.level as level from eindex e "
 			+ "join term_hierarchy t on t.child_id=e.id "
-			+ "where t.parent_id in (select id from eindex where title like ':title%' and ismainterm=true order by title) "
-			+ "and t.level=1 and e.code is not null order by e.title asc", nativeQuery = true)
+			+ "where t.parent_id in (select id from eindex where title like ?1% and ismainterm=true order by title) "
+			+ "and t.level in (0,1) order by e.title asc", nativeQuery = true)
 	List<Map<String,Object>> searchMainTermLevelOne(String title);
 
 	@Query(value = "select e.id as id,e.title as title,e.code as code,e.see as see,e.seealso as seealso,e.seecat as seecat,e.nemod as nemod,e.ismainterm as ismainterm,t.parent_id as parentId,t.child_id as childId,t.level as level "
 			+ "from eindex e "
 			+ "join term_hierarchy t on t.parent_id=e.id "
-			+ "where t.child_id in (select id from eindex where title like ':title%' and ismainterm=false)", nativeQuery = true)
+			+ "where t.child_id in (select id from eindex where title like ?1% and ismainterm=false)", nativeQuery = true)
 	List<Map<String,Object>> searchLevelTermMainTerm(String title);
 	
-	@Query(value="SELECT e from eindex e where e.title = :title% and ismainterm=true",nativeQuery = true)
+	@Query(value="SELECT e from eindex e where e.title like ?1% and ismainterm=true",nativeQuery = true)
 	List<Eindex> findMainTerm(String title);
 	
-	@Query(value="SELECT e from eindex e join term_hierarchy t on t.child_id=e.id where t.parent_id in (select id from eindex where title in(:firstMainTerms) and ismainterm=true) and e.title = :title%",nativeQuery = true)
+	@Query(value="SELECT e from eindex e join term_hierarchy t on t.child_id=e.id where t.parent_id in (select id from eindex where title in(?1) and ismainterm=true) and e.title like ?2%",nativeQuery = true)
 	List<Eindex> findSecondMainTermLevel(List<String> firstMainTerms, String title);
 }
