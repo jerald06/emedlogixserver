9e6b967d0429f81bef174b7132262f7c9607bf19
jeral <jerald19960606@gmail.com>
2023-10-06 23:50:47 +0530

    new commit

--- src/main/java/com/emedlogix/repository/ESAlterTermRepository.java
+++ src/main/java/com/emedlogix/repository/ESAlterTermRepository.java
@@ -11,7 +11,8 @@ import java.util.Optional;
 @Repository
 public interface ESAlterTermRepository extends ElasticsearchRepository<AlterTerm, String> {
     @Query("{\"bool\": {\"must\": [{\"match\": {\"alterDescription\": {\"query\": \"?0\", \"fuzziness\": \"AUTO\"}}}], \"filter\": [{\"term\": {\"version\": \"?1\"}}]}}")
-    List<AlterTerm> findByAlterDescriptionAndVersion(String query,String version);
+    List<AlterTerm> findByAlterDescriptionAndVersion(String query, String version);
+
 
     Optional<AlterTerm> findByCode(String code);
 }
